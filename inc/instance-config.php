0d7f594ef49a3b1e943f849bf0667386e024e72b
paulkass <pkassianik@gmail.com>
2020-05-22 23:31:29 -0700

    Added db config

--- inc/instance-config.php
+++ inc/instance-config.php
@@ -8,16 +8,21 @@
  *  You can copy values from config.php (defaults) and paste them here.
  */
 
-
+    
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
 
 	// Database stuff
 	$config['db']['type']		= 'mysql';
-	$config['db']['server']		= 'localhost';
-	$config['db']['user']		= '';
-	$config['db']['password']	= '';
-	$config['db']['database']	= '';
+	$config['db']['server']		= $server;
+	$config['db']['user']		= $username;
+	$config['db']['password']	= $password;
+	$config['db']['database']	= $db;
 	
-	//$config['root']				= '/';
+	$config['root']				= '/';
 	
 	
 ?>
